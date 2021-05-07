7d723d430e296f7364d765e4a38aa90b807becd3
nparashar150 <nparashar150@gmail.com>
2021-05-08 00:39:50 +0530

    Add create.html Use localStorage to create notes Delete notes from localStorage

--- public/app.js
+++ public/app.js
@@ -1,16 +1,57 @@
-var cube = document.querySelector('.cube');
-var radioGroup = document.querySelector('.radio-group');
-var currentClass = '';
+console.log('Create');
+makeNotes();
 
-function changeSide() {
-    var checkedRadio = radioGroup.querySelector(':checked');
-    var showClass = 'show-' + checkedRadio.value;
-    if (currentClass) {
-        cube.classList.remove(currentClass);
+function createNote() {
+    let addText = document.getElementById('noteTextArea');
+    let notes = localStorage.getItem("notes");
+    if (notes == null) {
+        notesObj = [];
+    } else {
+        notesObj = JSON.parse(notes);
     }
-    cube.classList.add(showClass);
-    currentClass = showClass;
+    notesObj.push(addText.value);
+    localStorage.setItem("notes", JSON.stringify(notesObj));
+    addText.value = "";
+    console.log(notesObj);
+    makeNotes();
 }
-changeSide();
 
-radioGroup.addEventListener('change', changeSide);
+function makeNotes() {
+    let notes = localStorage.getItem("notes");
+    if (notes == null) {
+        notesObj = [];
+    } else {
+        notesObj = JSON.parse(notes);
+    }
+    let nukestring = "";
+    notesObj.forEach(function(element, index) {
+        nukestring += `
+                <div class="card text-dark m-3 mb-3" style="max-width: 18rem;">
+            <div class="card-header">Note-${index + 1}</div>
+            <div class="card-body">
+                <p class="card-text">${element}
+                </p>
+                <button type="submit" class="mt-3 btn btn-create text-white mb-2" onclick="deleteNote()">Delete Note-${index + 1}</button>
+            </div>
+        </div>`;
+    });
+    let notesElement = document.getElementById('notes');
+    if (notesObj.length != 0) {
+        notesElement.innerHTML = nukestring;
+    } else {
+        notesElement.innerHTML = `<p class="container d-flex justify-content-center align-items-center text-danger">Create your first note now....</p>`
+    }
+}
+
+function deleteNote(index) {
+    console.log('Deleting', index);
+    let notes = localStorage.getItem("notes");
+    if (notes == null) {
+        notesObj = [];
+    } else {
+        notesObj = JSON.parse(notes);
+    }
+    notesObj.splice(index, 1);
+    localStorage.setItem("notes", JSON.stringify(notesObj));
+    makeNotes();
+}
